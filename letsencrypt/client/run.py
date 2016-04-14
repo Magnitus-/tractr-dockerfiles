f2ff9dd04df84fa6971b597175aaf7db5b39662e
Eric Vallee <eric_vallee@webificservices.com>
2016-04-14 13:54:22 -0400

    Made letsencrypt non-interactive with the --agree-tos flag

--- letsencrypt/client/run.py
+++ letsencrypt/client/run.py
@@ -6,11 +6,8 @@ import traceback
 import logging
 
 while True:
-    #subprocess.check_call(["/opt/letsencrypt/venv/bin/letsencrypt", "certonly", "--test-cert"])
     try:
-        ps = subprocess.Popen(('echo', '"A\n"'), stdout=subprocess.PIPE)
-        output = subprocess.check_output(("/opt/letsencrypt/venv/bin/letsencrypt", "certonly", "--test-cert"), stdin=ps.stdout)
+        subprocess.check_call(["/opt/letsencrypt/venv/bin/letsencrypt", "certonly", "--test-cert", "--agree-tos"])
     except Exception as e:
         logging.error(traceback.format_exc())
-    ps.wait()
     time.sleep(86400)
