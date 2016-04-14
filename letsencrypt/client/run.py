9fcb3ed55a0c4333b668041844e06bb11c6076d6
Eric Vallee <eric_vallee@webificservices.com>
2016-04-14 15:14:09 -0400

    Added missing argument

--- letsencrypt/client/run.py
+++ letsencrypt/client/run.py
@@ -11,7 +11,7 @@ while True:
         if os.path.isfile("/etc/letsencrypt/live/privkey.pem"):
             subprocess.check_call(["/opt/letsencrypt/venv/bin/letsencrypt", "renew", "--dry-run", "--agree-tos", "--webroot", "--webroot-path", "/var/www/html/letsencrypt"])
         else:
-            subprocess.check_call(["/opt/letsencrypt/venv/bin/letsencrypt", "certonly", "--test-cert", "--agree-tos", "--standalone"])
+            subprocess.check_call(["/opt/letsencrypt/venv/bin/letsencrypt", "certonly", "--test-cert", "--agree-tos", "--standalone", "--standalone-supported-challenges", "http-01"])
     except Exception as e:
         logging.error(traceback.format_exc())
     time.sleep(86400)
