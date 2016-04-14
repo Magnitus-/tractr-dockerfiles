947315f7217fdd64f9fd92a9344b5aad1f42ae9c
Eric Vallee <eric_vallee@webificservices.com>
2016-04-14 14:54:14 -0400

    Added logic to run on standalone server for initial certificate generation

--- letsencrypt/client/run.py
+++ letsencrypt/client/run.py
@@ -2,12 +2,16 @@
 # -*- coding: utf-8 -*-
 import time
 import subprocess
+import os
 import traceback
 import logging
 
 while True:
     try:
-        subprocess.check_call(["/opt/letsencrypt/venv/bin/letsencrypt", "certonly", "--test-cert", "--agree-tos"])
+        if os.path.isfile("/etc/letsencrypt/live/privkey.pem"):
+            subprocess.check_call(["/opt/letsencrypt/venv/bin/letsencrypt", "renew", "--dry-run", "--agree-tos", "--webroot", "--webroot-path", "/var/www/html/letsencrypt"])
+        else:
+            subprocess.check_call(["/opt/letsencrypt/venv/bin/letsencrypt", "certonly", "--test-cert", "--agree-tos", "--standalone"])
     except Exception as e:
         logging.error(traceback.format_exc())
     time.sleep(86400)
